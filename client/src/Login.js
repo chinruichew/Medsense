99d2ac4238a2b67de5a27c8c412192b441de8d29
rickypf <rcky8@outlook.com>
2017-10-13 23:30:52 +0800

    Login

---
+++ client/src/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+    }
+
+    handleUserChange(e) {
+        this.setState({ username: e.target.value });
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
+
+    handleLogin(e) {
+        console.log(this.state.username);
+        console.log(this.state.password);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text"
+                    placeholder="Enter Username"
+                    value={this.state.username}
+                    onChange={(e) => this.handleUserChange(e)} />
+
+                <input type="text"
+                    placeholder="Enter Password"
+                    value={this.state.password}
+                    onChange={(e) => this.handlePasswordChange(e)} />
+
+                <button type="button"
+                    onClick={(e) => this.handleLogin(e)}>
+                    Login </button>
+            </div>
+        )
+    }
+}
+
+export default Login;
