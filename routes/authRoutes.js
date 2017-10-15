cdd2a8ce3bf8a7c5aaef218e7cb71951dff9206b
chinrui <chinrui@transcelestial.com>
2017-10-15 12:25:58 +0800

    Configured Route calls

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+const passport = require('passport');
+
+module.exports = app => {
+    /* Start of Local Auth */
+    app.get(
+        '/auth/local',
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        })
+    );
+};
