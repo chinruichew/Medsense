ab222e4aed68abca274f7b35fe75344b17dc0355
chinrui <chinrui@transcelestial.com>
2017-10-24 20:33:51 +0800

    Updated component to show login on run

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+import Login from './LoginSignup/Login';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Login exact path="/" component={Login} />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
