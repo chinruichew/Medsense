8a71ad5278cea6d5845b16f1a5e85610c54b048f
rickypf <rcky8@outlook.com>
2017-10-26 11:14:28 +0800

    Sign Up

--- routes/signup/index.js
+++ routes/signup/index.js
@@ -7,9 +7,23 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 var User = require('../../models/User');
 
-router.get('/findOne', function (req, res) {
-    User.findOne({ username: "admin1" }, function (err, user) {
-        return res.send({ message: user });
+router.post('/', function (req, res) {
+    User.findOne({ username: req.body.username }, function (err, user) {
+        if (err) { return res.send(err) }
+        if (user) {
+            return res.status(409).send({ data: null, message: "user exists" });  
+        }
+        if (!user) {
+            var newUser = new User();
+            newUser.username = req.body.username;
+            newUser.password = req.body.password;
+            newUser.school = req.body.school;
+            newUser.year = req.body.year;
+            newUser.profilepicture = req.body.profilepicture;
+            newUser.usertype = req.body.usertype;
+            newUser.save();
+            return res.status(201).send({ data: user, message: "user created" });
+        }
     });
 });
 
