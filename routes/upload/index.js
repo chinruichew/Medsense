561c10cc63c4b9d08a678dd47f999345df8801d3
rickypf <rcky8@outlook.com>
2017-10-28 20:33:04 +0800

    Update Case Backend

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -32,32 +32,60 @@ router.post('/', function (req, res) {
             reference: jsonObject[prop]['reference'],
             stem: jsonObject[prop]['stem']
         })
-
-
         for (var key in jsonObject[prop]['mcqs']) {
-            //console.log(key, jsonObject[prop]['mcqs'][key]);
             var newMCQ = new MCQ({
                 answer: "",
                 status: true
             })
             for (var key1 in jsonObject[prop]['mcqs'][key]) {
-                //console.log(key1, jsonObject[prop]['mcqs'][key][key1]);
-                if (key1 == "answer") {
-                    newMCQ.answer = jsonObject[prop]['mcqs'][key][key1];
-                } else {
-                    newMCQ.status = jsonObject[prop]['mcqs'][key][key1];
-                }
+                newMCQ.answer = jsonObject[prop]['mcqs'][key]["answer"];
+                newMCQ.status = jsonObject[prop]['mcqs'][key]["status"];
             }
             newMCQ.save();
-            newQuestion.mcqs.push(newMCQ._id)
+            newQuestion.mcqs.push(newMCQ._id);
             newQuestion.save();
-
         }
-        newCase.questions.push(newQuestion._id)
+        newCase.questions.push(newQuestion._id);
         newCase.save();
     }
-
     return res.status(201).send({ data: null, message: "case created" });
+});
+
+router.post('/update', function (req, res) {
+    Case.findById(req.body.id, function (err, oneCase) {
+        oneCase.title = "req.body.title"
+        oneCase.difficulty = "req.body.difficulty"
+        oneCase.speciality = "req.body.speciality"
+        oneCase.subspeciality = "req.body.subspeciality"
+        oneCase.approach = "req.body.aproach"
+        oneCase.scenario = "req.body.scenario"
+
+        var questionArray = req.body.questionArray;
+        var jsonObject = JSON.parse(questionArray);
+        for (var prop in jsonObject) {
+            Question.findById(jsonObject[prop]['id'], function (err, oneQuestion) {
+                oneQuestion.title = jsonObject[prop]['title']
+                oneQuestion.type = jsonObject[prop]['type']
+                oneQuestion.open = jsonObject[prop]['open']
+                oneQuestion.pearl = jsonObject[prop]['pearl']
+                oneQuestion.timelimit = jsonObject[prop]['timelimit']
+                oneQuestion.reference = jsonObject[prop]['reference']
+                oneQuestion.stem = jsonObject[prop]['stem']
+
+                for (var key in jsonObject[prop]['mcqs']) {
+                    MCQ.findById(jsonObject[prop]['mcqs'][key]["id"], function (err, oneMCQ) {
+                        if (oneMCQ) {
+                            oneMCQ.answer = jsonObject[prop]['mcqs'][key]["answer"]
+                            oneMCQ.status = jsonObject[prop]['mcqs'][key]["status"];
+                            oneMCQ.save();
+                        }
+                    })
+                }
+            })
+        }
+        oneCase.save()
+        return res.status(201).send({ data: null, message: "case updated" });
+    });
 });
 
 module.exports = router;
