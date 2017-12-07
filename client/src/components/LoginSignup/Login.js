f2b41b58971176afb43590719d6225dada887440
chinrui <chinrui@transcelestial.com>
2017-12-07 16:52:18 +0800

    Implemented Force Redirect for Login and Sign Up

--- client/src/components/LoginSignup/Login.js
+++ client/src/components/LoginSignup/Login.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import {connect} from "react-redux";
 
 class Login extends Component {
     componentDidMount() {
@@ -7,40 +9,59 @@ class Login extends Component {
         document.body.style.backgroundSize = "100% 1200px";
     }
 
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="main-login main-center">
-                        <img src="./medsense_logo.png" style={{height: '120px', width: '300px'}} />
-                        <form className="form-horizontal" method="post" action="/auth/local">
-                            <div className="form-group">
-                                <label for="username" className="cols-sm-2 control-label">Username</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="main-login main-center">
+                                <img src="./medsense_logo.png" style={{height: '120px', width: '300px'}} />
+                                <form className="form-horizontal" method="post" action="/auth/local">
+                                    <div className="form-group">
+                                        <label for="username" className="cols-sm-2 control-label">Username</label>
+                                        <div className="cols-sm-10">
+                                            <div className="input-group">
+                                                <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
+                                                <input type="text" className="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className="form-group">
-                                <label for="password" className="cols-sm-2 control-label">Password</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-                                        <input type="password" className="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                                    <div className="form-group">
+                                        <label for="password" className="cols-sm-2 control-label">Password</label>
+                                        <div className="cols-sm-10">
+                                            <div className="input-group">
+                                                <span className="input-group-addon"><i className="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                                <input type="password" className="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
+                                    <div className="form-group ">
+                                        <button type="submit" className="btn btn-primary btn-lg btn-block login-button">Login</button>
+                                    </div>
+                                </form>
                             </div>
-                            <div className="form-group ">
-                                <button type="submit" className="btn btn-primary btn-lg btn-block login-button">Login</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                );
+            default:
+                return(<Redirect to="/home" />);
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                {this.renderContent()}
             </div>
-        )
+        );
     }
 }
 
-export default Login;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Login);
--- client/src/components/LoginSignup/StudentSignup.js
+++ client/src/components/LoginSignup/StudentSignup.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { Redirect } from 'react-router-dom';
+
 import StudentSignUpForm from './StudentSignUpForm';
+import {connect} from "react-redux";
 
 class StudentSignup extends Component {
     constructor(props) {
@@ -28,6 +30,21 @@ class StudentSignup extends Component {
         this.setState({ year: e.target.value });
     }
 
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return(
+                    <div className="row">
+                        <StudentSignUpForm username={this.state.username} password={this.state.password} confirm_password={this.state.confirm_password} school={this.state.school} year={this.state.year} />
+                    </div>
+                );
+            default:
+                return(<Redirect to="/home" />);
+        }
+    }
+
     render() {
         let imagepreview;
         if (this.state.profilepicture) {
@@ -38,15 +55,16 @@ class StudentSignup extends Component {
             );
         }
 
-
         return (
             <div className="container-fluid">
-                <div className="row">
-                    <StudentSignUpForm username={this.state.username} password={this.state.password} confirm_password={this.state.confirm_password} school={this.state.school} year={this.state.year} />
-                </div>
+                {this.renderContent()}
             </div>
         )
     }
 }
 
-export default StudentSignup;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(StudentSignup);
