67ff8d62b17b91997ce684a64b0912b5e7bdc8a0
chinrui <chinrui@transcelestial.com>
2017-12-30 19:41:16 +0800

    added logger and dev tool middleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -12,7 +12,20 @@ import registerServiceWorker from './registerServiceWorker';
 
 import reducers from './reducers/index'
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(logger, reduxThunk)));
 
 ReactDOM.render(
     <Provider store={store}><App /></Provider>,
