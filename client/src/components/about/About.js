f19716e1a23b682b168fc3a8669e320ebaa741b0
chinrui <chinrui@transcelestial.com>
2018-01-03 18:04:59 +0800

    refactored about page

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -71,7 +71,7 @@ class About extends Component {
 
     render() {
         return(
-            <div className="container-fluid">
+            <div className="container">
                 <div className="row">
                     <div className="col-md-12 text-center">
                         <h1>
