fbea7bf6946b314d55357ae9d3e9154e8c050908
chinrui <chinrui@transcelestial.com>
2018-01-03 20:13:18 +0800

    aws deploy

--- client/src/components/Profile/UploadProfilePicture.js
+++ client/src/components/Profile/UploadProfilePicture.js
@@ -3,20 +3,16 @@ import { bindAll } from 'lodash';
 import axios from 'axios';
 
 class UploadProfilePicture extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: null
-        };
-        bindAll(this, 'onFileUploadChange', 'profilePictureUpload');
-    }
+    state = {
+        file: null
+    };
 
     onFormSubmit = (e) => {
         e.preventDefault();
         this.profilePictureUpload(this.state.file);
     };
 
-    profilePictureUpload(file){
+    profilePictureUpload = (file) => {
         const formData = new FormData();
         formData.append('file',file);
         const config = {
@@ -31,19 +27,21 @@ class UploadProfilePicture extends Component {
             });
         });
 
-    }
+    };
 
-    onFileUploadChange(e) {
+    onFileUploadChange = (e) => {
         this.setState({file:e.target.files[0]});
-    }
+    };
 
     render() {
         return(
             <div className="main-login main-center">
                 <img src="./medsense_logo.png" style={{height: '120px', width: '300px'}} />
+                {/*Note*/}
                 <form onSubmit={this.onFormSubmit} className="form-horizontal" method="post" action="/api/uploadProfileImage" encType="multipart/form-data">
                     <div className="form-group">
                         <label>Upload a profile picture:</label>
+                        {/*Note this?*/}
                         <input id="profile_picture" type="file" name="upload" multiple="multiple" onChange={this.onFileUploadChange} />
                     </div>
                     <button type="submit" className="btn btn-primary btn-lg btn-block login-button">Submit</button>
