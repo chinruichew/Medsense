910cdbcbb57b33ce6739255566402bba1c69df1c
chinrui <chinrui@transcelestial.com>
2018-01-16 22:54:54 +0800

    aws deploy

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ s3.getObject(getParams, function(err, data) {
             chalkAnimation.rainbow("DB connection successful!");
             setTimeout(() => {
                 console.log(chalk.green.underline.bold('Initializing...'));
-            }, 300);
+            }, 500);
         });
 
     });
