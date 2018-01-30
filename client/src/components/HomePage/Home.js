1b2357ba73304c27b93f9a3a0ec6ba90acbcd2ed
Chin Rui <chewchinrui@gmail.com>
2018-01-30 17:39:03 +0800

    aws deploy

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Button, Table} from 'react-bootstrap';
-import {NavLink, Redirect} from 'react-router-dom';
+import { Redirect} from 'react-router-dom';
 import axios from 'axios';
 
 import './Home.css';
