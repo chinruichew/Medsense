6e537f78d8cd8cf3cb9545de2f9bb9127eb203ba
rickypf <rcky8@outlook.com>
2018-02-06 22:42:05 +0800

    NLP

--- routes/nlpRoutes.js
+++ routes/nlpRoutes.js
@@ -5,22 +5,54 @@ const Question = require('../models/Question');
 
 const constants = require('../utility/constantTypes');
 
+var natural = require('natural');
+
 module.exports = app => {
     app.post('/api/matchNLP', async (req, res) => {
-        console.log(req.body.id)
-        Question.find({
-            "_id": req.body.id
-        }, function (err, question) {
-            console.log(question)
-        })
-        // const cases = await Case.findOne({ _id: req.body.values.vetId }).select().populate({
-        //     path: 'questions',
-        //     model: 'questions'
-        // });
-        // res.send(cases);
-        res.send("hello")
+        sw = require('stopword')
+        const answerString = 'UECr - check renal function for yearly scans'.split(' ')
+        const answerString1 = sw.removeStopwords(answerString)
+
+        //remove unwanted words
+        const oldString = 'check function renal in scans years'.split(' ')
+        const studentString = sw.removeStopwords(oldString)
+
+        // root word
+        natural.PorterStemmer.attach();
+        var stuarr = []
+        for (var i in studentString) {
+            stuarr.push(studentString[i].stem())
+        }
+        console.log("stuarr " + stuarr)
+
+        var ansarr =[]
+        for (var i in answerString1) {
+            ansarr.push(answerString1[i].stem())
+        }
+        console.log("ansarr " + ansarr)
+
+        //build up dictionary trie
+        var Trie = natural.Trie;
+        var trie = new Trie();
+        trie.addStrings(stuarr)
+
+        var counter = 0;
+
+        for(var i in ansarr) {
+            if(trie.contains(ansarr[i])) {
+                //console.log(ansarr[i])
+                counter++;
+            } else {
+                console.log(ansarr[i])
+                console.log(trie.findPrefix(ansarr[i]))
+                if(trie.findPrefix(ansarr[i])[0] != null) {
+                    counter++;
+                }
+            }
+        }
+
+        console.log(counter);
+
     });
 
 };
-
-
