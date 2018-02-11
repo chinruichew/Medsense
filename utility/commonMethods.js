ed337a64f84c47b7ba2f2c591f4cbd66a0bf9d1f
Chin Rui <chewchinrui@gmail.com>
2018-02-11 22:02:06 +0800

    Edited date formatter

--- utility/commonMethods.js
+++ utility/commonMethods.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DATE_FORMATTER: function(date) {
+    ISO_DATE_FORMATTER: function(date) {
         return new Date(date.substring(0, date.length - 1) + '-08:00');
     }
 };
