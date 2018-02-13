41dcfc7f62c254abfae438d308e881629345f7b5
Chin Rui <chewchinrui@gmail.com>
2018-02-13 15:59:36 +0800

    Edited NLP routes

--- routes/nlpRoutes.js
+++ routes/nlpRoutes.js
@@ -5,51 +5,51 @@ const Question = require('../models/Question');
 
 const constants = require('../utility/constantTypes');
 
-var natural = require('natural');
-var unique = require('array-unique');
-var sw = require('stopword');
+const natural = require('natural');
+const unique = require('array-unique');
+const sw = require('stopword');
 
 module.exports = app => {
     app.post('/api/matchNLP', async (req, res) => {
-        var counter = ""
-        var studentAnswer = req.body.values.openEnded;
+        let counter = "";
+        let studentAnswer = req.body.values.openEnded;
         studentAnswer = studentAnswer.replace(/<[^>]*>/g, '');
-        var originalAnswer = "";
+        let originalAnswer = "";
         Question.find({ "_id": req.body.id }, function (req, res) {
             originalAnswer = res[0]['openEnded'];
             originalAnswer = originalAnswer.replace(/<[^>]*>/g, '');
             console.log(res[0]['openEnded'])
-        })
+        });
         setTimeout(function () {
-            var tokenizer = new natural.WordTokenizer();
+            const tokenizer = new natural.WordTokenizer();
 
-            const studentAnswerStop = sw.removeStopwords(tokenizer.tokenize(studentAnswer))
-            const originalAnswerStop = sw.removeStopwords(tokenizer.tokenize(originalAnswer))
+            const studentAnswerStop = sw.removeStopwords(tokenizer.tokenize(studentAnswer));
+            const originalAnswerStop = sw.removeStopwords(tokenizer.tokenize(originalAnswer));
             console.log(studentAnswerStop);
-            console.log(originalAnswerStop)
+            console.log(originalAnswerStop);
 
             // root word
             natural.PorterStemmer.attach();
-            var studentAnswerArray = []
-            for (var i in studentAnswerStop) {
-                studentAnswerArray.push(studentAnswerStop[i].stem())
+            const studentAnswerArray = [];
+            for (let i in studentAnswerStop) {
+                studentAnswerArray.push(studentAnswerStop[i].stem());
             }
-            console.log(studentAnswerArray)
+            console.log(studentAnswerArray);
 
-            var originalAnswerArray = []
-            for (var i in originalAnswerStop) {
-                originalAnswerArray.push(originalAnswerStop[i].stem())
+            const originalAnswerArray = [];
+            for (let i in originalAnswerStop) {
+                originalAnswerArray.push(originalAnswerStop[i].stem());
             }
-            console.log(originalAnswerArray)
+            console.log(originalAnswerArray);
 
             //build up dictionary trie
-            var Trie = natural.Trie;
-            var trie = new Trie();
-            trie.addStrings(unique(studentAnswerArray))
+            const Trie = natural.Trie;
+            const trie = new Trie();
+            trie.addStrings(unique(studentAnswerArray));
 
-            var counter = 0;
+            let counter = 0;
 
-            for (var i in unique(originalAnswerArray)) {
+            for (let i in unique(originalAnswerArray)) {
                 if (trie.contains(unique(originalAnswerArray)[i])) {
                     counter++;
                 } else {
@@ -58,10 +58,10 @@ module.exports = app => {
                     // }
                 }
             }
-            console.log(counter)
-            console.log(originalAnswerArray.length)
+            console.log(counter);
+            console.log(originalAnswerArray.length);
             console.log(counter / originalAnswerArray.length);
-            counter = counter / originalAnswerArray.length
+            counter = counter / originalAnswerArray.length;
              res.send({ "data": counter })
         }.bind(this), 500);
 
