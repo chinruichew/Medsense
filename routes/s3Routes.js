22dc60184c0693d39a298eb1835b60b9a713673b
Chin Rui <chewchinrui@gmail.com>
2018-03-10 11:34:24 +0800

    Refactored attachment s3 upload methods

--- routes/s3Routes.js
+++ routes/s3Routes.js
@@ -30,6 +30,7 @@ module.exports = app => {
                                 res.send("done");
                             } else {
                                 User.update({_id: req.session.user._id}, {profilepicture: "https://s3-ap-southeast-1.amazonaws.com/profile-picture-images/" + req.session.user._id + "/user_profile" + (req.session.user.profilepictureVersion + 1) + ".jpg", profilepictureVersion: req.session.user.profilepictureVersion + 1 }, function (err, response) {
+                                    // Do not delete - production logging
                                     console.log("Successfully uploaded data to profile-picture-images/" + req.session.user._id + "/user_profile" + (req.session.user.profilepictureVersion + 1) + ".jpg");
                                     res.send("done");
                                 });
@@ -54,7 +55,6 @@ module.exports = app => {
             const objID = fields.objID;
             const myBucket = 'case-upload-attachments';
             const s3 = new aws.S3();
-            console.log(files.file);
             if (files.file!==undefined && files.file!==null){
                 const file = files.file[0];
 
@@ -67,10 +67,10 @@ module.exports = app => {
                     };
                     s3.putObject(params, function (err, data) {
                         if (err) {
-                            console.log(err);
-                            res.send("done");
+                            throw(err);
                         } else {
                             Question.update({_id: objID}, {attachment: "https://s3-ap-southeast-1.amazonaws.com/case-upload-attachments/" + caseID + "/question" + qID + ".jpg"}, function (err, response) {
+                                // Do not delete - production logging
                                 console.log("Successfully uploaded data to case-upload-attachments/" + caseID + "/question" + qID + ".jpg");
                                 res.send("done");
                             });
@@ -90,7 +90,7 @@ module.exports = app => {
                         const params = {Bucket: myBucket, Delete: {Objects:[{Key:caseID + "/question" + qID + ".jpg"}]}};
                         s3.deleteObjects(params, function(err, data) {
                             if (err) {
-                                // console.log(err);
+                                throw(err);
                             }
                         });
                     }
@@ -108,7 +108,7 @@ module.exports = app => {
             const caseID = fields.caseID;
             const qID = fields.qID;
             const objID = fields.objID;
-            const myBucket = 'case-upload-attachments';
+            const myBucket = 'case-pearl-upload-attachments';
             const s3 = new aws.S3();
             if (files.file!==undefined && files.file!==null){
                 const file = files.file[0];
@@ -116,7 +116,7 @@ module.exports = app => {
                 fs.readFile(file.path, function (err, data) {
                     const params = {
                         Bucket: myBucket,
-                        Key: caseID + "/question" + qID + "/pearls.jpg",
+                        Key: caseID + "/question" + qID + ".jpg",
                         Body: data,
                         ACL: 'public-read'
                     };
@@ -124,8 +124,9 @@ module.exports = app => {
                         if (err) {
                             throw(err);
                         } else {
-                            Question.update({_id: objID}, {pearlAttachment: "https://s3-ap-southeast-1.amazonaws.com/case-upload-attachments/" + caseID + "/question" + qID + "/pearls.jpg"}, function (err, response) {
-                                console.log("Successfully uploaded data to case-upload-attachments/" + caseID + "/question" + qID + "/pearls.jpg");
+                            Question.update({_id: objID}, {pearlAttachment: "https://s3-ap-southeast-1.amazonaws.com/case-pearl-upload-attachments/" + caseID + "/question" + qID + ".jpg"}, function (err, response) {
+                                // Do not delete - production logging
+                                console.log("Successfully uploaded data to case-pearl-upload-attachments/" + caseID + "/question" + qID + ".jpg");
                                 res.send("done");
                             });
                         }
@@ -135,16 +136,16 @@ module.exports = app => {
                 Question.update({_id: objID}, {pearlAttachment: ""}, function (err, response) {
                     console.log(response);
                 });
-                const params = {Bucket: myBucket, Key: caseID + "/question" + qID + "/pearls.jpg"};
+                const params = {Bucket: myBucket, Key: caseID + "/question" + qID + ".jpg"};
                 s3.getObject(params, function(err, data) {
                     // Handle any error and exit
                     if (err) {
                         // console.log(err);
                     } else {
-                        const params = {Bucket: myBucket, Delete: {Objects:[{Key:caseID + "/question" + qID + "/pearls.jpg"}]}};
+                        const params = {Bucket: myBucket, Delete: {Objects:[{Key:caseID + "/question" + qID + ".jpg"}]}};
                         s3.deleteObjects(params, function(err, data) {
                             if (err) {
-                                // console.log(err);
+                                throw(err);
                             }
                         });
                     }
