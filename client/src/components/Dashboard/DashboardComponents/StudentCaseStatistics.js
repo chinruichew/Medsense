273bf1e28926582e661c83fea57af3aacc2bd0ad
Chin Rui <chewchinrui@gmail.com>
2018-03-11 15:11:47 +0800

    aws deploy

--- client/src/components/Dashboard/DashboardComponents/StudentCaseStatistics.js
+++ client/src/components/Dashboard/DashboardComponents/StudentCaseStatistics.js
@@ -14,14 +14,18 @@ class StudentCaseStatistics extends Component {
             );
         });
 
-        const answers = this.state.answers.map(answer => {
+        const answers = this.state.answers.map((answer, index) => {
             console.log(answer);
+            let placeholderImage = <Image circle src="/individual_case_image.jpg" style={{height: '150px', width: '150px'}} />;
+            if(index % 2 === 0) {
+                placeholderImage = <Image circle src="/individual_case_image_2.jpg" style={{height: '150px', width: '150px'}} />;
+            }
             return(
                 <div key={answer._id} className="col-md-4">
                     <NavLink to={"/caseStatsIndividual?case=" + answer._id}>
                         <div className="card">
                             <div className="card-content text-center">
-                                <Image circle src="/individual_case_image.jpg" style={{height: '150px', width: '150px'}} />
+                                {placeholderImage}
                                 <h4>{answer.case.title}</h4>
                                 <p>Speciality: {answer.case.speciality}</p>
                                 <p>Attempt {answer.attempt}</p>
