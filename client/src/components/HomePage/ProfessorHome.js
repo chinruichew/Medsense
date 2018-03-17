0dffe3763f4f9dda5c15752a1b4001a207e44a93
HUANG MINGRUI _ <mrhuang.2015@smu.edu.sg>
2018-03-17 17:28:04 +0800

    Changed homepage UI (hardcoded for cases)

--- client/src/components/HomePage/ProfessorHome.js
+++ client/src/components/HomePage/ProfessorHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {Button, Table} from 'react-bootstrap';
+import {Button, Carousel, Col} from 'react-bootstrap';
 import {NavLink} from "react-router-dom";
+import "./carousel.css";
 
 class ProfessorHome extends Component {
     state = {
@@ -31,73 +32,79 @@ class ProfessorHome extends Component {
                     }
                     toVet = false;
                 }
+                let imgUrl = './homepage.png'
                 return(
                     <div className="container-fluid">
                         <div className="text-center">
-                            <h2>Welcome back to Medsense</h2>
-                        </div><br/><br/>
-                        <div className="row">
-                            <div className="col-sm-offset-3 col-sm-3 text-center" style={{fontSize:'150%'}}>
-                                <NavLink to='/upload'>
-                                    <Button style={{background: "white", color: 'black', width: "15em", height: "11em"}} bsSize="large">
-                                        <img className="left-picture" src="./upload.png" alt="" style={{height:"5em", marginBottom: "3%"}}/>
-                                        <br/>Upload a case <br/>
-                                        <p style={{color:"#1aa3ff", marginTop:"2%", fontWeight:"bold"}}>Share your knowledge</p>
-                                    </Button>
-                                </NavLink>
-                            </div>
-                            <div className="col-sm-3 text-center" style={{fontSize:'150%'}}>
-                                <NavLink to='/vetting'>
-                                    <Button style={{background: "white", color: 'black', width: "15em", height: "11em"}} bsSize="large">
-                                        <img src="./vet.png" alt="" style={{height:"5em", marginBottom: "3%"}}/>
-                                        <br/>Vet a case<br/>
-                                        <p style={{color:"#1aa3ff", marginTop:"2%", fontWeight:"bold"}}> <em>You have {numCasesPending} pending cases</em></p>
-                                    </Button>
-                                </NavLink>
-                            </div>
+                            <h1 style={{color: "#199ED8", fontWeight: "bold", marginTop: "0"}}>Pending Cases</h1>
+                        </div>
+
+                        <Carousel style={{height: "25em"}} className="carousel">
+                            <Carousel.Item style={{paddingLeft: "12%", paddingTop: "3%"}}>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                            </Carousel.Item>
+                            <Carousel.Item style={{paddingLeft: "12%", paddingTop: "3%"}}>
+                                <Button style={{background: "white", color: 'black', width: "15em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "15em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                            </Carousel.Item>
+                        </Carousel>
+
+                        <div style={{backgroundImage: 'url(' + imgUrl + ')', backgroundSize: 'cover',
+                            height: "350px", textAlign: "center", color: "white"}}>
+                            <h1 style={{paddingTop: "7%", fontWeight: "bold"}}>Upload Case</h1>
+                            <hr style={{borderColor: "white", borderWidth: "5px", marginTop: "1%", width: "70%"}}/>
+                            <p>Description of case upload</p><br/>
+                            <NavLink to='/upload'>
+                                <Button style={{background: "#199ED8", color: 'white', border: "0", width: "8em",
+                                    height: "3em", verticalAlign: "center"}}>
+                                    <h4 style={{padding: "0", margin: "0"}}>Upload</h4>
+                                </Button>
+                            </NavLink>
                         </div>
                         <br/>
-                        <div className="row">
-                            <strong><h3 style={{fontStyle:"italic", marginBottom:"1%"}}>Upload Recommendations</h3></strong>
-                            <Table responsive>
-                                <thead >
-                                <tr style={{background: '#82C5D9', fontSize:'130%'}}>
-                                    <th><center>S/N</center></th>
-                                    <th><center>Approach</center></th>
-                                    <th><center>Sub-speciality</center></th>
-                                    <th><center>Difficulty</center></th>
-                                    <th><center>Recommended Time Limit</center></th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr align="center">
-                                    <td>1</td>
-                                    <td>Cough</td>
-                                    <td>Respiratory & Critical Care Medicine</td>
-                                    <td>Beginner</td>
-                                    <td>5min (MCQ)<br/>12min (Open-ended)</td>
-                                    <td><Button  type="button" bsStyle="primary">Upload</Button></td>
-                                </tr>
-                                <tr align="center">
-                                    <td>2</td>
-                                    <td>Dizziness</td>
-                                    <td>Respiratory & Critical Care Medicine</td>
-                                    <td>Advanced</td>
-                                    <td>5min (MCQ)<br/>8min (Open-ended)</td>
-                                    <td><Button  type="button" bsStyle="primary">Upload</Button></td>
-                                </tr>
-                                <tr align="center">
-                                    <td>3</td>
-                                    <td>Palpitations</td>
-                                    <td>Cardiology</td>
-                                    <td>Beginner</td>
-                                    <td>2min (MCQ)<br/>10min (Open-ended)</td>
-                                    <td><Button  type="button" bsStyle="primary">Upload</Button></td>
-                                </tr>
-                                </tbody>
-                            </Table>
+                        <div >
+                            <Col sm={6} style={{backgroundColor: "#F7F7F7", height: "180px"}}>
+                                <h3 style={{marginLeft: "3%", paddingTop: "3%"}}>FOLLOW US</h3>
+                                <a href="https://www.facebook.com/medsense/">
+                                    <img src="./facebook.png" style={{width: "3em", marginLeft: "8%" }}/>
+                                </a>
+                            </Col>
+                            <Col smOffset={6} style={{backgroundColor: "#F7F7F7", height: "180px"}}>
+                                <h3 style={{marginLeft: "79%", paddingTop: "5%"}}>VERSION</h3>
+                                <h5 style={{marginLeft: "70%", fontSize: "130%", textAlign: "center"}}>Version 2.0
+                                    <br/>Last Updated: 1 April 2018
+                                </h5>
+                            </Col>
                         </div>
+
+                        <div style={{margin: "2%"}}>Copyright © Medsense, 2018</div>
                     </div>
                 );
         }
--- client/src/components/HomePage/StudentHome.js
+++ client/src/components/HomePage/StudentHome.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import {Button, Table} from 'react-bootstrap';
+import {Button, Carousel, Col} from 'react-bootstrap';
 import {NavLink} from "react-router-dom";
 import axios from 'axios';
-
 import BootstrapModal from '../UI/Modal/UploadBootstrapModal.js';
+import "./carousel.css";
 
 class StudentHome extends Component {
     state = {
@@ -48,77 +48,82 @@ class StudentHome extends Component {
             case null:
                 return;
             default:
+                let imgUrl = './homepage.png'
                 return(
-                    <div className="container-fluid">
+                    <div>
                         <div className="text-center">
-                            <h2>Welcome back to Medsense</h2>
-                        </div><br/><br/>
-                        <div className="row">
-                            <div className="col-sm-offset-3 col-sm-3 text-center" style={{fontSize:'150%'}}>
-                                <NavLink to='/upload'>
-                                    <Button style={{background: "white", color: 'black', width: "15em", height: "11em"}} bsSize="large">
-                                        <img src="./upload.png" alt="" style={{height:"5em", marginBottom: "3%"}}/>
-                                        <br/>Upload a case<br/>
-                                        <p style={{color:"#1aa3ff", marginTop:"2%", fontWeight:"bold"}}>Share your experiences</p>
-                                    </Button>
-                                </NavLink>
-                            </div>
-                            <div className="col-sm-3  text-center" style={{fontSize:'150%'}}>
-                                <NavLink to='/game'>
-                                    <Button style={{background: "white", color: 'black', width: "15em", height: "11em"}} bsSize="large">
-                                        <img src="./challenge.png" alt="" style={{height:"5em", marginBottom: "3%"}}/>
-                                        <br/>Start doing a case<br/>
-                                        <p style={{color:"#1aa3ff", marginTop:"2%", fontWeight:"bold"}}> <em>There are {this.state.pendingCases.length} new cases</em></p>
-                                    </Button>
-                                </NavLink>
-                            </div>
+                            <h1 style={{color: "#199ED8", fontWeight: "bold", marginTop: "0"}}>Recommended Cases</h1>
+                        </div>
+
+                        <Carousel style={{height: "25em"}} className="carousel">
+                            <Carousel.Item style={{paddingLeft: "12%", paddingTop: "3%"}}>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "14em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                            </Carousel.Item>
+                            <Carousel.Item style={{paddingLeft: "12%", paddingTop: "3%"}}>
+                                <Button style={{background: "white", color: 'black', width: "15em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+                                <Button style={{background: "white", color: 'black', width: "15em", height: "13em", marginRight: "8%"}} bsSize="large">
+                                    <img src="./approach1.jpg" alt="" style={{height:"8em"}}/>
+                                    <h3 style={{fontWeight: "bold", margin: "0"}}>Case Title</h3>
+                                    <h4 style={{margin: "0"}}>Speciality</h4>
+                                    <h4 style={{margin: "0"}}>Sub-speciality</h4>
+                                </Button>
+
+                            </Carousel.Item>
+                        </Carousel>
+
+                        <div style={{backgroundImage: 'url(' + imgUrl + ')', backgroundSize: 'cover',
+                            height: "350px", textAlign: "center", color: "white"}}>
+                            <h1 style={{paddingTop: "7%", fontWeight: "bold"}}>Upload Case</h1>
+                            <hr style={{borderColor: "white", borderWidth: "5px", marginTop: "1%", width: "70%"}}/>
+                            <p>Description of case upload</p><br/>
+                            <NavLink to='/upload'>
+                                <Button style={{background: "#199ED8", color: 'white', border: "0", width: "8em",
+                                    height: "3em", verticalAlign: "center"}}>
+                                    <h4 style={{padding: "0", margin: "0"}}>Upload</h4>
+                                </Button>
+                            </NavLink>
                         </div>
                         <br/>
-                        <div className="row">
-                            <strong><h3 style={{fontStyle:"italic", marginBottom:"1%"}}>Game Recommendations</h3></strong>
-                            <Table responsive>
-                                <thead>
-                                <tr style={{background: '#82C5D9', fontSize:'130%'}}>
-                                    <th><center>Case Title</center></th>
-                                    <th><center>Approaches</center></th>
-                                    <th><center>Speciality</center></th>
-                                    <th><center>Sub-speciality</center></th>
-                                    <th><center>Difficulty</center></th>
-                                    <th><center>Last Updated</center></th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr align="center">
-                                    <td>Approach to Abdominal Pain</td>
-                                    <td>Abdominal Pain</td>
-                                    <td>Medicine</td>
-                                    <td>Endocrinology</td>
-                                    <td>Beginner</td>
-                                    <td>05 Jan 2018<br/>4:51pm</td>
-                                    <td><Button  type="button" bsStyle="primary">Try</Button></td>
-                                </tr>
-                                <tr align="center">
-                                    <td>Severe Migraine</td>
-                                    <td>Headache</td>
-                                    <td>Medicine</td>
-                                    <td>Neurology</td>
-                                    <td>Beginner</td>
-                                    <td>04 Feb 2018<br/>10:41pm</td>
-                                    <td><Button  type="button" bsStyle="primary">Try</Button></td>
-                                </tr>
-                                <tr align="center">
-                                    <td>Blood in Stools</td>
-                                    <td>Gastrointestinal Bleed</td>
-                                    <td>Surgery</td>
-                                    <td>Colorectal</td>
-                                    <td>Beginner</td>
-                                    <td>01 Feb 2018<br/>9:41pm</td>
-                                    <td><Button  type="button" bsStyle="primary">Try</Button></td>
-                                </tr>
-                                </tbody>
-                            </Table>
+                        <div >
+                            <Col sm={6} style={{backgroundColor: "#F7F7F7", height: "180px"}}>
+                                <h3 style={{marginLeft: "3%", paddingTop: "3%"}}>FOLLOW US</h3>
+                                <a href="https://www.facebook.com/medsense/">
+                                    <img src="./facebook.png" style={{width: "3em", marginLeft: "8%" }}/>
+                                </a>
+                            </Col>
+                            <Col smOffset={6} style={{backgroundColor: "#F7F7F7", height: "180px"}}>
+                                <h3 style={{marginLeft: "79%", paddingTop: "5%"}}>VERSION</h3>
+                                <h5 style={{marginLeft: "70%", fontSize: "130%", textAlign: "center"}}>Version 2.0
+                                    <br/>Last Updated: 1 April 2018
+                                </h5>
+                            </Col>
+                        </div>
 
+                        <div style={{margin: "2%"}}>Copyright © Medsense, 2018</div>
+
+                        <div>
                             <BootstrapModal
                                 show={this.state.showModal}
                                 onHide={(e) => this.setState({ showModal: false })}
