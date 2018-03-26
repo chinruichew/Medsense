67757fdc7ac9b70101dca50d436737d2854c385a
rickypf <rcky8@outlook.com>
2018-03-26 16:21:25 +0800

    Admin Analytics

--- client/src/components/HomePage/AdminHome.js
+++ client/src/components/HomePage/AdminHome.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
 import { Button, Table } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
+import axios from 'axios'
 
 class AdminHome extends Component {
     state = {
-
+        caseCount: "",
+        specialityCount: ""
     };
+
+    componentDidMount() {
+        axios.get('/api/fetchCount').then(res => {
+            console.log(res)
+            this.setState({
+                caseCount: res.data
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+        axios.post('/api/fetchSpecialityCount').then(res => {
+            console.log(res)
+            this.setState({
+                specialityCount: res.data
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 
     render() {
 
         return (
             <div className="container-fluid">
                 <div className="text-center">
-                    <h2>Welcome back to Medsense</h2>
-                </div><br /><br />
+                    {/* <h2>Welcome back to Medsense</h2> */}
+                </div>
+                <div className="row">
+                    row1
+                </div>
+                <div className="row">
+                    <div class="col-lg-6">{this.state.caseCount.length}</div>
+                    <div class="col-lg-6">{this.state.specialityCount.length}</div>
+                </div>
             </div>
         );
 
