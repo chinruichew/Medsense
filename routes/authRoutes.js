afa2ccc5896133675fc5f08c70758b2971a8093d
Chin Rui <chewchinrui@gmail.com>
2018-04-05 02:36:00 +0800

    Revert "Fixed auth tracking bug"
    This reverts commit 903a39e882a87d0bef48ff7d563b61feb5ae9541.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -38,9 +38,7 @@ module.exports = app => {
     app.get('/api/current_user', (req, res) => {
         if(req.session.user !== undefined && req.session.user !== '' && req.session.user) {
             User.findByIdAndUpdate(req.session.user._id, { $set: { currentLogin: new Date() }}, { new: true }, function(err, user) {
-                if(user !== null) {
-                    console.log('Current User Id:', user._id, ', Current User Username:', user.username);
-                }
+                console.log('Current User Id:', user._id, ', Current User Username:', user.username);
                 res.send(req.session.user);
             });
         } else {
