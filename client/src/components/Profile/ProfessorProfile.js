07294f98f9c1346bdd447daa56d456d181a13ccd
qiminwang <qimin.wang.2015@sis.smu.edu.sg>
2018-04-09 13:40:26 +0800

    formatting

--- client/src/components/Profile/ProfessorProfile.js
+++ client/src/components/Profile/ProfessorProfile.js
@@ -71,7 +71,7 @@ class ProfessorProfile extends Component {
 
     render() {
         let subSpeciality = this.state.subspeciality.map((obj, index) => {
-           return <li key={index}>{obj}</li>;
+           return <p key={index}>{obj}</p>;
         });
 
         const popover = (
@@ -114,9 +114,9 @@ class ProfessorProfile extends Component {
                             <td style={{width: '100px'}} ><center>
                                 <h4> {this.state.speciality} </h4>
                             </center></td>
-                            <td style={{width: '100px'}} >
-                                <h4><ul>{subSpeciality}</ul></h4>
-                            </td>
+                            <td style={{width: '100px'}} ><center>
+                                <h4> {subSpeciality} </h4>
+                            </center></td>
                             {contributionRank}
                         </tr>
                     </Table>
