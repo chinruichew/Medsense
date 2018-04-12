27e73709071a20e33f8abc86808a6819ebaa53a8
Chin Rui <chewchinrui@gmail.com>
2018-04-12 20:45:35 +0800

    Revert "Schema Fix"
    This reverts commit 78d16706ef52b199df2144e6d2143c7565f6b4d3.

--- models/Subspeciality.js
+++ models/Subspeciality.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const subspecialitySchema = new Schema({
-    speciality: { type: String, default: "" },
-    subspeciality: { type: String, default: "" }
+    subspeciality: { type: String, default: "" },
 });
 
 const subspecialityModel = mongoose.model('subspecialities', subspecialitySchema);
