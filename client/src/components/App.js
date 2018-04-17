7a3b507c9b63e63aff3e99ae2841b6e9ae9b16e1
Chin Rui <chewchinrui@gmail.com>
2018-04-17 16:23:32 +0800

    aws deploy

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -34,7 +34,7 @@ class App extends Component {
     render() {
         return (
             <BrowserRouter>
-                <HttpsRedirect>
+                <div>
                     <Header />
                     <Switch>
                         <Route exact path="/login" component={Login} />
@@ -53,7 +53,7 @@ class App extends Component {
                         <Route exact path="/about" component={Acknowledgement}/>
                         <Route path="/" component={NotFound} />
                     </Switch>
-                </HttpsRedirect>
+                </div>
             </BrowserRouter>
         );
     }
